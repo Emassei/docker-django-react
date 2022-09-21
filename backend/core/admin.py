99f9321005cef0fa410012408040cda3432db4f0
Ernie Massei <Emassei@users.noreply.github.com>
2022-09-21 17:01:53 -0500

    add blog model

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
 from .models import User
+from .models import Post
 
 
 @admin.register(User)
 class UserAdmin(DefaultUserAdmin):
     pass
+
+admin.site.register(Post)
